3d79b91fe7bf07012c9d2764834cbab4d5bcd5da
Miguel-T80c <the80scomputers@gmail.com>
2020-04-08 01:59:15 +0200

    Joystick DB9MD Support

--- TSConf.sv
+++ TSConf.sv
@@ -117,14 +117,23 @@ module emu
 	// 1 - D-/TX
 	// 2..6 - USR2..USR6
 	// Set USER_OUT to 1 to read from USER_IN.
-	input   [6:0] USER_IN,
-	output  [6:0] USER_OUT,
+	output	USER_OSD,
+	output	USER_MODE,
+	input	[7:0] USER_IN,
+	output	[7:0] USER_OUT,
 
 	input         OSD_STATUS
 );
 
 assign ADC_BUS  = 'Z;
-assign USER_OUT = '1;
+
+wire   joy_split, joy_mdsel;
+wire   [5:0] joy_in = {USER_IN[6],USER_IN[3],USER_IN[5],USER_IN[7],USER_IN[1],USER_IN[2]};
+assign USER_OUT  = |status[31:30] ? {3'b111,joy_split,3'b111,joy_mdsel} : '1;
+assign USER_MODE = |status[31:30] ;
+assign USER_OSD  = joydb9md_1[7] & joydb9md_1[5]; // A�adir esto para OSD
+
+
 assign VGA_F1 = 0;
 assign {UART_RTS, UART_TXD, UART_DTR} = 0;
 
@@ -140,25 +149,26 @@ assign VIDEO_ARY = status[5] ? 8'd9  : 8'd3;
 localparam CONF_STR = {
 	"TSConf;;",
 	"S,VHD,Mount virtual SD;",
-	"-;",
+"-;",
 	"O5,Aspect ratio,4:3,16:9;",
 	"O12,Scandoubler Fx,None,HQ2x,CRT 25%,CRT 50%;",
-	"-;",
+	"OUV,Serial SNAC DB9MD,Off,1 Player,2 Players;",
+"-;",
 	"O34,Stereo mix,None,25%,50%,100%;",
 	"OST,General Sound,512KB,1MB,2MB;",
-	"-;",
+"-;",
 	"OU,CPU Type,NMOS,CMOS;",
 	"O67,CPU Speed,3.5MHz,7MHz,14MHz;",
 	"O8,CPU Cache,On,Off;",
 	"O9A,#7FFD span,128K,128K Auto,1024K,512K;",
 	"OLN,ZX Palette,Default,B.black,Light,Pale,Dark,Grayscale,Custom;",
 	"OPR,INT Offset,1,2,3,4,5,6,7,0;",
-	"-;",
+"-;",
 	"OBD,F11 Reset,boot.$C,sys.rom,ROM;",
 	"OEF,           bank,TR-DOS,Basic 48,Basic 128,SYS;",
 	"OGI,Shift+F11 Reset,ROM,boot.$C,sys.rom;",
 	"OJK,           bank,Basic 128,SYS,TR-DOS,Basic 48;",
-	"-;",
+"-;",
 	"R0,Reset and apply settings;",
 	"J,Fire 1,Fire 2;",
 	"V,v",`BUILD_DATE
@@ -203,8 +213,8 @@ end
 
 
 //////////////////   HPS I/O   ///////////////////
-wire  [5:0] joy_0;
-wire  [5:0] joy_1;
+wire  [5:0] joy_0_USB;
+wire  [5:0] joy_1_USB;
 wire [15:0] joya_0;
 wire [15:0] joya_1;
 wire  [1:0] buttons;
@@ -234,6 +244,40 @@ wire  [7:0] ioctl_dout;
 wire        ioctl_download;
 wire  [7:0] ioctl_index;
 
+
+wire [5:0] joy_0 = |status[31:30] ? {
+	joydb9md_1[4], // btn_fire2	-> 5 * B
+	joydb9md_1[6], // btn_fire1	-> 4 * A
+	joydb9md_1[3], // btn_up	-> 3 * U
+	joydb9md_1[2], // btn_down	-> 2 * D
+	joydb9md_1[1], // btn_left	-> 1 * L
+	joydb9md_1[0], // btn_righ	-> 0 * R 
+	} 
+	: joy_0_USB;
+
+wire [5:0] joy_1 =  status[31]    ? {
+	joydb9md_2[4], // btn_fire2	-> 5 * B
+	joydb9md_2[6], // btn_fire1	-> 4 * A
+	joydb9md_2[3], // btn_up	-> 3 * U
+	joydb9md_2[2], // btn_down	-> 2 * D
+	joydb9md_2[1], // btn_left	-> 1 * L
+	joydb9md_2[0], // btn_right	-> 0 * R 
+	} 
+	: status[30] ? joy_0_USB : joy_1_USB;
+
+
+reg [15:0] joydb9md_1,joydb9md_2;
+joy_db9md joy_db9md
+(
+  .clk       ( clk_sys    ), //35-50MHz
+  .joy_split ( joy_split  ),
+  .joy_mdsel ( joy_mdsel  ),
+  .joy_in    ( joy_in     ),
+  .joystick1 ( joydb9md_1 ),
+  .joystick2 ( joydb9md_2 )	  
+);
+
+
 hps_io #(.STRLEN($size(CONF_STR)>>3)) hps_io
 (
 	.clk_sys(clk_sys),
@@ -241,8 +285,8 @@ hps_io #(.STRLEN($size(CONF_STR)>>3)) hps_io
 
 	.conf_str(CONF_STR),
 
-	.joystick_0(joy_0),
-	.joystick_1(joy_1),
+	.joystick_0(joy_0_USB),
+	.joystick_1(joy_1_USB),
 	.joystick_analog_0(joya_0),
 	.joystick_analog_1(joya_1),
 
@@ -253,6 +297,7 @@ hps_io #(.STRLEN($size(CONF_STR)>>3)) hps_io
 	.RTC(RTC),
 
 	.ps2_mouse(ps2_mouse),
+	.joy_raw({joydb9md_1[4],joydb9md_1[6],joydb9md_1[3:0]}),
 	.ps2_key(ps2_key),
 
 	.sd_lba(sd_lba),
